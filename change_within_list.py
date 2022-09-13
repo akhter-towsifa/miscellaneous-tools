e4497f05def69cc270dda24118620b418b93caf5
Towsifa Akhter <51368122+akhter-towsifa@users.noreply.github.com>
2022-09-13 14:22:39 -0500

    Update change_within_list.py

--- change_within_list.py
+++ change_within_list.py
@@ -1,5 +1,18 @@
 '''
-
+This file reads a .list file or .txt file that already has name of files
+line by line. All this file does is concatenates the string path name to
+each file line by line.
 
 --Towsifa Akhter
 '''
+
+f = open("Run2022BCv1.list", "r")
+
+#print(f.read(10)) #print first 10 characters of the file
+
+list_of_original_lines = f.readlines()
+list_of_new_lines = ["root://cms-xrd-global.cern.ch/" + line.strip() +"\n" for line in list_of_original_lines]
+
+f = open("Run2022BCv1.list", "w")
+f.writelines(list_of_new_lines)
+f.close()
